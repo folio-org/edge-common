99166dbef2e9b502b798c01cdb24a22beb402aea
mreno-EBSCO@users.noreply.github.com <mreno-EBSCO@users.noreply.github.com>
2018-10-30 14:49:52 -0400

    Created EdgeVerticle2 that supports DeploymentOptions for config
    The existing `EdgeVerticle` creates a vertx object in its constructor. This causes the `AbstractVerticle` to lose its vertx, which is supplied by the launch script. The launch vertx has the context where the deployment options are provided to the verticle, so deployment options are not available with `EdgeVerticle`.
    
    To get around this, `EdgeVerticle2` was created. The constructor no longer creates a vertx and all operations are now done in the verticle `start` method using the launch provided vertx instance. The `start` method is provided by the `AbstractVerticle` and we use this method to perform our potentially long setup operations (secure store loading, etc.) that could block the main thread. Other entry points to the verticle behave synchronously, so are not ideal for this purpose.
    
    Default deployment options are loaded by the verticle. These can be overridden by env variable, which can be further be overridden by deployment options provided by the launcher.

--- NEWS.md
+++ NEWS.md
@@ -1,2 +1,9 @@
+10/30/2018
+ - Added an edge verticle implementation that allows deployment options to be
+   set by the verticle launcher. This required moving much of the code from
+   the constructor into the `start` method. This allows the launcher to provide
+   a vertx to the verticle which contains the deployment options. It also allows
+   the verticle to perform time consuming tasks without blocking the main
+   thread.
 08/16/2018
  - The source of the API Key is now configurable via the `api_key_sources` system property.  See [README.md](README.md) for details.
---
+++ src/main/java/org/folio/edge/core/EdgeVerticle2.java
@@ -0,0 +1,126 @@
+package org.folio.edge.core;
+
+import static org.folio.edge.core.Constants.DEFAULT_SECURE_STORE_TYPE;
+import static org.folio.edge.core.Constants.PROP_SECURE_STORE_TYPE;
+import static org.folio.edge.core.Constants.SYS_API_KEY_SOURCES;
+import static org.folio.edge.core.Constants.SYS_LOG_LEVEL;
+import static org.folio.edge.core.Constants.SYS_NULL_TOKEN_CACHE_TTL_MS;
+import static org.folio.edge.core.Constants.SYS_OKAPI_URL;
+import static org.folio.edge.core.Constants.SYS_PORT;
+import static org.folio.edge.core.Constants.SYS_REQUEST_TIMEOUT_MS;
+import static org.folio.edge.core.Constants.SYS_SECURE_STORE_PROP_FILE;
+import static org.folio.edge.core.Constants.SYS_SECURE_STORE_TYPE;
+import static org.folio.edge.core.Constants.SYS_TOKEN_CACHE_CAPACITY;
+import static org.folio.edge.core.Constants.SYS_TOKEN_CACHE_TTL_MS;
+import static org.folio.edge.core.Constants.TEXT_PLAIN;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.folio.edge.core.cache.TokenCache;
+import org.folio.edge.core.security.SecureStore;
+import org.folio.edge.core.security.SecureStoreFactory;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+public abstract class EdgeVerticle2 extends AbstractVerticle {
+
+  private static final Logger logger = Logger.getLogger(EdgeVerticle2.class);
+
+  private static Pattern isURL = Pattern.compile("(?i)^http[s]?://.*");
+
+  protected SecureStore secureStore;
+
+  @Override
+  public void start(Future<Void> future) {
+    JsonObject jo = Constants.DEFAULT_DEPLOYMENT_OPTIONS.copy();
+    config().mergeIn(jo.mergeIn(config()));
+
+    final int port = config().getInteger(SYS_PORT);
+    logger.info("Using port: " + port);
+
+    final String logLvl = config().getString(SYS_LOG_LEVEL);
+    Logger.getRootLogger().setLevel(Level.toLevel(logLvl));
+    logger.info("Using log level: " + logLvl);
+
+    logger.info("Using okapi URL: " + config().getString(SYS_OKAPI_URL));
+    logger.info("Using API key sources: " + config().getString(SYS_API_KEY_SOURCES));
+
+    final long cacheTtlMs = config().getLong(SYS_TOKEN_CACHE_TTL_MS);
+    logger.info("Using token cache TTL (ms): " + cacheTtlMs);
+
+    final long failureCacheTtlMs = config().getLong(SYS_NULL_TOKEN_CACHE_TTL_MS);
+    logger.info("Using token cache TTL (ms): " + failureCacheTtlMs);
+
+    final int cacheCapacity = config().getInteger(SYS_TOKEN_CACHE_CAPACITY);
+    logger.info("Using token cache capacity: " + cacheCapacity);
+
+    logger.info("Using request timeout (ms): " + config().getLong(SYS_REQUEST_TIMEOUT_MS));
+
+    // initialize the TokenCache
+    TokenCache.initialize(cacheTtlMs, failureCacheTtlMs, cacheCapacity);
+
+    secureStore = initializeSecureStore(config().getString(SYS_SECURE_STORE_PROP_FILE));
+
+    final HttpServer server = getVertx().createHttpServer();
+
+    final Router router = defineRoutes();
+
+    server.requestHandler(router::accept).listen(port, result -> {
+      if (result.succeeded()) {
+        future.complete();
+      } else {
+        future.fail(result.cause());
+      }
+    });
+  }
+
+  public abstract Router defineRoutes();
+
+  protected SecureStore initializeSecureStore(String secureStorePropFile) {
+    Properties secureStoreProps = new Properties();
+
+    if (secureStorePropFile != null) {
+      URL url = null;
+      try {
+        if (isURL.matcher(secureStorePropFile).matches()) {
+          url = new URL(secureStorePropFile);
+        }
+
+        try (InputStream in = url == null ? new FileInputStream(secureStorePropFile) : url.openStream()) {
+          secureStoreProps.load(in);
+          logger.info("Successfully loaded properties from: " +
+              secureStorePropFile);
+        }
+      } catch (Exception e) {
+        logger.warn("Failed to load secure store properties.", e);
+      }
+    } else {
+      logger.warn("No secure store properties file specified.  Using defaults");
+    }
+
+    // Order of precedence: system property, properties file, default
+    String type = config().getString(SYS_SECURE_STORE_TYPE,
+        secureStoreProps.getProperty(PROP_SECURE_STORE_TYPE, DEFAULT_SECURE_STORE_TYPE));
+
+    return SecureStoreFactory.getSecureStore(type, secureStoreProps);
+  }
+
+  protected void handleHealthCheck(RoutingContext ctx) {
+    ctx.response()
+      .setStatusCode(200)
+      .putHeader(HttpHeaders.CONTENT_TYPE, TEXT_PLAIN)
+      .end("\"OK\"");
+  }
+}
