25e88340d75b02f95ea97a877bd8241bbe97107a
Adam Dickmeiss <adam@indexdata.dk>
2021-01-22 19:47:36 +0100

    Avoid deprecated org.mockito.Matchers.any (#40)

--- src/test/java/org/folio/edge/core/security/AwsParamStoreTest.java
+++ src/test/java/org/folio/edge/core/security/AwsParamStoreTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import java.util.Properties;
