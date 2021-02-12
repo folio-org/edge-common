76cf6c709d3b1fd8f96a9e57f18bbba80e46334b
Adam Dickmeiss <adam@indexdata.dk>
2021-02-12 18:49:51 +0100

    Allow non-HTTP server EDGCOMMON-31 (#46)
    I started by making tweaks to EdgeVerticle.. Then decided it was better to just offer two verticles again.
    
    EdgeVerticle is based on EdgeVerticleCore
    
    This change is backwards compatible for EdgeVerticle.
    
    But protected initializeSecureStore method changed signature to return Future<SecureStore>, in order to
    ensure Verticle is failing if secure store property file can not be found.
    
    initializeSecureStore is not used anywhere, except in edge-common itself.

---
+++ src/main/java/org/folio/edge/core/EdgeVerticleCore.java
@@ -0,0 +1,104 @@
+package org.folio.edge.core;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.folio.edge.core.cache.TokenCache;
+import org.folio.edge.core.security.SecureStore;
+import org.folio.edge.core.security.SecureStoreFactory;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+import static org.folio.edge.core.Constants.*;
+import static org.folio.edge.core.Constants.SYS_SECURE_STORE_PROP_FILE;
+
+/**
+ * Verticle for Edge module with no built-in HTTP listener.
+ */
+public class EdgeVerticleCore extends AbstractVerticle {
+  private static final Logger logger = LogManager.getLogger(EdgeVerticleCore.class);
+
+  protected SecureStore secureStore;
+
+  private static final Pattern isURL = Pattern.compile("(?i)^http[s]?://.*");
+
+  @Override
+  public void start(Promise<Void> promise) {
+    JsonObject jo = Constants.DEFAULT_DEPLOYMENT_OPTIONS.copy();
+    config().mergeIn(jo.mergeIn(config()));
+
+    final String logLvl = config().getString(SYS_LOG_LEVEL);
+    Configurator.setRootLevel(Level.toLevel(logLvl));
+    logger.info("Using log level: {}", logLvl);
+
+    logger.info("Using okapi URL: {}", config().getString(SYS_OKAPI_URL));
+    logger.info("Using API key sources: {}", config().getString(SYS_API_KEY_SOURCES));
+
+    final long cacheTtlMs = config().getLong(SYS_TOKEN_CACHE_TTL_MS);
+    logger.info("Using token cache TTL (ms): {}", cacheTtlMs);
+
+    final long failureCacheTtlMs = config().getLong(SYS_NULL_TOKEN_CACHE_TTL_MS);
+    logger.info("Using token cache TTL (ms): {}", failureCacheTtlMs);
+
+    final int cacheCapacity = config().getInteger(SYS_TOKEN_CACHE_CAPACITY);
+    logger.info("Using token cache capacity: {}", cacheCapacity);
+
+    logger.info("Using request timeout (ms): {}", config().getLong(SYS_REQUEST_TIMEOUT_MS));
+
+    // initialize the TokenCache
+    TokenCache.initialize(cacheTtlMs, failureCacheTtlMs, cacheCapacity);
+
+    initializeSecureStore(config().getString(SYS_SECURE_STORE_PROP_FILE))
+      .onSuccess(res -> {
+        secureStore = res;
+        promise.complete();
+      })
+      .onFailure(promise::fail);
+  }
+
+  protected Future<SecureStore> initializeSecureStore(String secureStorePropFile) {
+    return getProperties(secureStorePropFile).compose(secureStoreProps -> {
+      // Order of precedence: system property, properties file, default
+      String type = config().getString(SYS_SECURE_STORE_TYPE,
+        secureStoreProps.getProperty(PROP_SECURE_STORE_TYPE, DEFAULT_SECURE_STORE_TYPE));
+
+      return Future.succeededFuture(SecureStoreFactory.getSecureStore(type, secureStoreProps));
+    });
+  }
+
+  static Future<Properties> getProperties(String secureStorePropFile) {
+    Properties secureStoreProps = new Properties();
+
+    if (secureStorePropFile != null) {
+      URL url = null;
+      try {
+        if (isURL.matcher(secureStorePropFile).matches()) {
+          url = new URL(secureStorePropFile);
+        }
+
+        try (
+          InputStream in = url == null ? new FileInputStream(secureStorePropFile) : url.openStream()) {
+          secureStoreProps.load(in);
+          logger.info("Successfully loaded properties from: " +
+            secureStorePropFile);
+        }
+      } catch (Exception e) {
+        logger.warn(e.getMessage(), e);
+        return Future.failedFuture("Failed to load secure store properties: " + e.getMessage());
+      }
+    } else {
+      logger.warn("No secure store properties file specified.  Using defaults");
+    }
+    return Future.succeededFuture(secureStoreProps);
+  }
+
+}
---
+++ src/test/java/org/folio/edge/core/EdgeVerticleCoreTest.java
@@ -0,0 +1,88 @@
+package org.folio.edge.core;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.folio.edge.core.utils.test.TestUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.folio.edge.core.Constants.SYS_PORT;
+import static org.folio.edge.core.Constants.SYS_SECURE_STORE_PROP_FILE;
+
+@RunWith(VertxUnitRunner.class)
+public class EdgeVerticleCoreTest {
+
+  static Vertx vertx;
+
+  @BeforeClass
+  public static void setUpOnce(TestContext context) {
+    vertx = Vertx.vertx();
+  }
+
+  @AfterClass
+  public static void tearDownOnce(TestContext context) {
+    vertx.close(context.asyncAssertSuccess());
+  }
+
+  @Test
+  public void testTcpServer(TestContext context) {
+    int serverPort = TestUtils.getPort();
+    JsonObject jo = new JsonObject()
+        .put(SYS_PORT, serverPort);
+
+    final DeploymentOptions opt = new DeploymentOptions().setConfig(jo);
+    TestVerticleTcpServer verticle = new TestVerticleTcpServer();
+    vertx.deployVerticle(verticle, opt).onComplete(context.asyncAssertSuccess(res ->
+      context.assertEquals(verticle.port, serverPort)
+    ));
+  }
+
+  public static class TestVerticleTcpServer extends EdgeVerticleCore {
+    int port;
+    @Override
+    public void start(Promise<Void> promise) {
+      Future.<Void>future(p -> super.start(p)).<Void>compose(res -> {
+        port = config().getInteger(SYS_PORT);
+        return vertx.createNetServer()
+          .connectHandler(socket ->
+            socket.close()
+          ).listen(port).mapEmpty();
+      }).onComplete(promise);
+    }
+  }
+
+  // test getProperties failure handling
+  // not doing it directly because we want to check that the Verticle reports failure
+  @Test
+  public void testGetPropertiesFailure1(TestContext context) {
+    JsonObject jo = new JsonObject()
+      .put(SYS_SECURE_STORE_PROP_FILE, "sx://foo.com");
+    final DeploymentOptions opt = new DeploymentOptions().setConfig(jo);
+    vertx.deployVerticle(new EdgeVerticleCore(), opt).onComplete(context.asyncAssertFailure(cause ->
+      context.assertEquals("Failed to load secure store properties: sx:/foo.com"
+        + " (No such file or directory)", cause.getMessage())
+    ));
+  }
+
+  // test getProperties failure handling
+  // not doing it directly because we want to check that the Verticle reports failure
+  @Test
+  public void testGetPropertiesFailure2(TestContext context) {
+    int serverPort = TestUtils.getPort();
+    JsonObject jo = new JsonObject()
+      .put(SYS_SECURE_STORE_PROP_FILE, "http://127.0.0.1:" + serverPort);
+    final DeploymentOptions opt = new DeploymentOptions().setConfig(jo);
+    vertx.deployVerticle(new EdgeVerticleCore(), opt).onComplete(context.asyncAssertFailure(cause ->
+      context.assertTrue(cause.getMessage().startsWith("Failed to load secure store properties: Connection refused"),
+        cause.getMessage())
+    ));
+  }
+
+}
