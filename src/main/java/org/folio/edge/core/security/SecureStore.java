20604202c3d474e734e02aa7dd3e5fe88531313f
Julian Ladisch <julianladisch@users.noreply.github.com>
2022-05-16 14:54:21 +0200

    EDGCOMMON-21: SecureStore#lookup = futurized get
    Use executeBlocking running on a worker thread so that the
    event loop doesn't gets blocked.

--- src/main/java/org/folio/edge/core/security/SecureStore.java
+++ src/main/java/org/folio/edge/core/security/SecureStore.java
@@ -1,5 +1,8 @@
 package org.folio.edge.core.security;
 
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 import java.util.Properties;
 
 public abstract class SecureStore {
@@ -10,7 +13,26 @@ public abstract class SecureStore {
     this.properties = properties;
   }
 
+  /**
+   * Fetch a value from the store.
+   *
+   * <p>In Vert.x based code use {@code #lookup(String, String, String) to avoid blocking the event loop.
+   */
   public abstract String get(String clientId, String tenant, String username) throws NotFoundException;
+
+  public Future<String> lookup(String clientId, String tenant, String username) {
+    Context context = Vertx.currentContext();
+    if (context == null) {
+      context = Vertx.vertx().getOrCreateContext();
+    }
+    return context.executeBlocking(run -> {
+      try {
+        run.tryComplete(get(clientId, tenant, username));
+      } catch (Throwable e) {
+        run.tryFail(e);
+      }
+    });
+  }
 
   public static class NotFoundException extends Exception {
 
