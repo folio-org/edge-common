b366970d31f13e9ce8712c3104ceeb7035b1a5d5
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2024-05-22 17:02:31 +0500

    [EDGORDERS-83-IT]. Add OkapiClient init WebClient TLS assertion

--- src/test/java/org/folio/edge/core/EdgeVerticleTlsIntegrationTest.java
+++ src/test/java/org/folio/edge/core/EdgeVerticleTlsIntegrationTest.java
@@ -10,9 +10,13 @@ import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import org.folio.edge.core.utils.OkapiClient;
+import org.folio.edge.core.utils.OkapiClientFactory;
+import org.folio.edge.core.utils.OkapiClientFactoryInitializer;
 import org.folio.edge.core.utils.SslConfigurationUtil;
 import org.folio.edge.core.utils.test.TestUtils;
 import org.junit.*;
@@ -20,17 +24,27 @@ import org.junit.runner.RunWith;
 
 import java.security.Security;
 
+import static org.folio.edge.core.Constants.SYS_LOG_LEVEL;
+import static org.folio.edge.core.Constants.SYS_OKAPI_URL;
+import static org.folio.edge.core.Constants.SYS_REQUEST_TIMEOUT_MS;
+import static org.folio.edge.core.Constants.SYS_SECURE_STORE_PROP_FILE;
+import static org.folio.edge.core.Constants.SYS_WEB_CLIENT_SSL_ENABLED;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(VertxUnitRunner.class)
 public class EdgeVerticleTlsIntegrationTest {
 
   private static final Logger logger = LogManager.getLogger(EdgeVerticleTlsIntegrationTest.class);
-  static Vertx vertx;
+  private static Vertx vertx;
 
-  static final String KEYSTORE_TYPE = "BCFKS";
-  static final String KEYSTORE_PATH = "test.keystore 1.bcfks";
-  static final String TRUST_STORE_PATH = "test.truststore 1.bcfks";
-  static final String KEYSTORE_PASSWORD = "SecretPassword";
-  static final String RESPONSE_MESSAGE = "<OK>";
+  private static final String KEYSTORE_TYPE = "BCFKS";
+  private static final String KEYSTORE_PATH = "test.keystore 1.bcfks";
+  private static final String TRUST_STORE_PATH = "test.truststore 1.bcfks";
+  private static final String KEYSTORE_PASSWORD = "SecretPassword";
+  private static final String RESPONSE_MESSAGE = "<OK>";
+  private static final String OKAPI_URL = "http://localhost:" + TestUtils.getPort();
+  private static final String TENANT = "diku";
 
   @BeforeClass
   public static void setUpOnce(TestContext context) {
@@ -44,7 +58,7 @@ public class EdgeVerticleTlsIntegrationTest {
   }
 
   @Test
-  public void testServerClientTlsCommunication(TestContext context) {
+  public void testServerClientTlsCommunication(TestContext context) throws IllegalAccessException {
     final JsonObject config = getCommonConfig();
 
     final HttpServerOptions serverOptions = new HttpServerOptions();
@@ -56,6 +70,13 @@ public class EdgeVerticleTlsIntegrationTest {
     httpServer
       .requestHandler(req -> req.response().putHeader("content-type", "text/plain").end(RESPONSE_MESSAGE))
       .listen(config.getInteger(Constants.SYS_PORT), http -> logger.info("Server started on port {}", config.getInteger(Constants.SYS_PORT)));
+
+    final OkapiClientFactory okapiClientFactory = OkapiClientFactoryInitializer.createInstance(vertx, config);
+    final OkapiClient okapiClient = okapiClientFactory.getOkapiClient(TENANT);
+    final WebClientOptions okapiWebClientOptions = (WebClientOptions) FieldUtils.readDeclaredField(okapiClient.client, "options", true);
+
+    assertTrue(okapiWebClientOptions.isSsl());
+    assertNotNull(okapiWebClientOptions.getTrustOptions());
 
     final WebClientOptions clientOptions = new WebClientOptions();
     clientOptions
@@ -80,6 +101,11 @@ public class EdgeVerticleTlsIntegrationTest {
   private JsonObject getCommonConfig() {
     int serverPort = TestUtils.getPort();
     return new JsonObject().put(Constants.SYS_PORT, serverPort)
+      .put(SYS_OKAPI_URL, OKAPI_URL)
+      .put(SYS_SECURE_STORE_PROP_FILE, "src/main/resources/ephemeral.properties")
+      .put(SYS_LOG_LEVEL, "TRACE")
+      .put(SYS_REQUEST_TIMEOUT_MS, 5000)
+      .put(SYS_WEB_CLIENT_SSL_ENABLED, false)
       .put(Constants.SYS_HTTP_SERVER_SSL_ENABLED, true)
       .put(Constants.SYS_HTTP_SERVER_KEYSTORE_TYPE, KEYSTORE_TYPE)
       .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PATH, KEYSTORE_PATH)
