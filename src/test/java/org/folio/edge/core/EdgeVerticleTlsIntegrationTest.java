93603a91c442a5b4d8d7c6d2f1b6f3d961fe5e0c
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2024-05-22 17:44:47 +0500

    [EDGORDERS-83-IT]. Add negative TLS integration test, remove unused BC deps

--- src/test/java/org/folio/edge/core/EdgeVerticleTlsIntegrationTest.java
+++ src/test/java/org/folio/edge/core/EdgeVerticleTlsIntegrationTest.java
@@ -24,7 +24,9 @@ import org.junit.runner.RunWith;
 
 import java.security.Security;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(VertxUnitRunner.class)
@@ -41,20 +43,33 @@ public class EdgeVerticleTlsIntegrationTest {
   private static final String OKAPI_URL = "http://localhost:" + TestUtils.getPort();
   private static final String TENANT = "diku";
 
-  @BeforeClass
-  public static void setUpOnce(TestContext context) {
+  @Before
+  public void setUpOnce(TestContext context) {
     Security.addProvider(new BouncyCastleFipsProvider());
     vertx = Vertx.vertx();
   }
 
-  @AfterClass
-  public static void tearDownOnce(TestContext context) {
+  @After
+  public void tearDown(TestContext context) {
     vertx.close(context.asyncAssertSuccess());
   }
 
   @Test
   public void testServerClientTlsCommunication(TestContext context) throws IllegalAccessException {
-    final JsonObject config = getCommonConfig();
+    int serverPort = TestUtils.getPort();
+    final JsonObject config = new JsonObject().put(Constants.SYS_PORT, serverPort)
+      .put(Constants.SYS_OKAPI_URL, OKAPI_URL)
+      .put(Constants.SYS_SECURE_STORE_PROP_FILE, "src/main/resources/ephemeral.properties")
+      .put(Constants.SYS_LOG_LEVEL, "TRACE")
+      .put(Constants.SYS_REQUEST_TIMEOUT_MS, 5000)
+      .put(Constants.SYS_HTTP_SERVER_SSL_ENABLED, true)
+      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_TYPE, KEYSTORE_TYPE)
+      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PATH, KEYSTORE_PATH)
+      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PASSWORD, KEYSTORE_PASSWORD)
+      .put(Constants.SYS_WEB_CLIENT_SSL_ENABLED, true)
+      .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_TYPE, KEYSTORE_TYPE)
+      .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PATH, TRUST_STORE_PATH)
+      .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PASSWORD, KEYSTORE_PASSWORD);
 
     final HttpServerOptions serverOptions = new HttpServerOptions();
     serverOptions.setPort(config.getInteger(Constants.SYS_PORT));
@@ -73,6 +88,45 @@ public class EdgeVerticleTlsIntegrationTest {
     assertTrue(webClientOptions.isSsl());
     assertNotNull(webClientOptions.getTrustOptions());
 
+    createClientRequest(context, webClientOptions, config);
+  }
+
+  @Test(expected = java.net.ConnectException.class)
+  public void testFailingServerClientTlsCommunication(TestContext context) throws IllegalAccessException {
+    int serverPort = TestUtils.getPort();
+    final JsonObject config = new JsonObject().put(Constants.SYS_PORT, serverPort)
+      .put(Constants.SYS_OKAPI_URL, OKAPI_URL)
+      .put(Constants.SYS_SECURE_STORE_PROP_FILE, "src/main/resources/ephemeral.properties")
+      .put(Constants.SYS_LOG_LEVEL, "TRACE")
+      .put(Constants.SYS_REQUEST_TIMEOUT_MS, 5000)
+      .put(Constants.SYS_HTTP_SERVER_SSL_ENABLED, true)
+      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_TYPE, KEYSTORE_TYPE)
+      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PATH, KEYSTORE_PATH)
+      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PASSWORD, KEYSTORE_PASSWORD)
+      .put(Constants.SYS_WEB_CLIENT_SSL_ENABLED, false);
+
+
+    final HttpServerOptions serverOptions = new HttpServerOptions();
+    serverOptions.setPort(config.getInteger(Constants.SYS_PORT));
+
+    SslConfigurationUtil.configureSslServerOptionsIfEnabled(config, serverOptions);
+
+    final HttpServer httpServer = vertx.createHttpServer(serverOptions);
+    httpServer
+      .requestHandler(req -> req.response().putHeader(HttpHeaders.CONTENT_TYPE, Constants.TEXT_PLAIN).end(RESPONSE_MESSAGE))
+      .listen(config.getInteger(Constants.SYS_PORT), http -> logger.info("Server started on port {}", config.getInteger(Constants.SYS_PORT)));
+
+    final OkapiClientFactory okapiClientFactory = OkapiClientFactoryInitializer.createInstance(vertx, config);
+    final OkapiClient okapiClient = okapiClientFactory.getOkapiClient(TENANT);
+    final WebClientOptions webClientOptions = (WebClientOptions) FieldUtils.readDeclaredField(okapiClient.client, "options", true);
+
+    assertFalse(webClientOptions.isSsl());
+    assertNull(webClientOptions.getTrustOptions());
+
+    createClientRequest(context, webClientOptions, config);
+  }
+
+  private static void createClientRequest(TestContext context, WebClientOptions webClientOptions, JsonObject config) {
     final WebClient webClient = WebClient.create(vertx, webClientOptions);
     webClient.get(config.getInteger(Constants.SYS_PORT), "localhost", "/")
       .send()
@@ -82,22 +136,5 @@ public class EdgeVerticleTlsIntegrationTest {
         context.assertEquals(HttpResponseStatus.OK.code(), response.statusCode());
         context.assertEquals(RESPONSE_MESSAGE, message);
       }));
-  }
-
-  private JsonObject getCommonConfig() {
-    int serverPort = TestUtils.getPort();
-    return new JsonObject().put(Constants.SYS_PORT, serverPort)
-      .put(Constants.SYS_OKAPI_URL, OKAPI_URL)
-      .put(Constants.SYS_SECURE_STORE_PROP_FILE, "src/main/resources/ephemeral.properties")
-      .put(Constants.SYS_LOG_LEVEL, "TRACE")
-      .put(Constants.SYS_REQUEST_TIMEOUT_MS, 5000)
-      .put(Constants.SYS_HTTP_SERVER_SSL_ENABLED, true)
-      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_TYPE, KEYSTORE_TYPE)
-      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PATH, KEYSTORE_PATH)
-      .put(Constants.SYS_HTTP_SERVER_KEYSTORE_PASSWORD, KEYSTORE_PASSWORD)
-      .put(Constants.SYS_WEB_CLIENT_SSL_ENABLED, true)
-      .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_TYPE, KEYSTORE_TYPE)
-      .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PATH, TRUST_STORE_PATH)
-      .put(Constants.SYS_WEB_CLIENT_TRUSTSTORE_PASSWORD, KEYSTORE_PASSWORD);
   }
 }
